befe9f5daca4a7b439e4d32ac8935dd204d67ca7
lxh15 <1392649188@qq.com>
2022-07-07 20:24:19 +0800

    Tabbar数据,插槽,tag徽标功能 完成

--- src/components/MyTable.vue
+++ src/components/MyTable.vue
@@ -3,23 +3,25 @@
     <!-- 表格标题区域 -->
     <thead>
       <tr>
-        <th>#</th>
+        <!-- <th>#</th>
         <th>商品名称</th>
         <th>价格</th>
         <th>标签</th>
-        <th>操作</th>
+        <th>操作</th> -->
+        <slot name="header"></slot>
       </tr>
     </thead>
     <!-- 表格主体区域 -->
     <tbody>
       <tr v-for="obj in arr" :key="obj.id">
-        <td>{{ obj.id }}</td>
+        <!-- <td>{{ obj.id }}</td>
         <td>{{ obj.goods_name }}</td>
         <td>{{ obj.goods_price }}</td>
         <td>{{ obj.tags }}</td>
         <td>
           <button class="btn btn-danger btn-sm">删除</button>
-        </td>
+        </td> -->
+        <slot name="table" :row="obj"></slot>
       </tr>
     </tbody>
   </table>
@@ -27,9 +29,16 @@
 
 <script>
 export default {
-  name: "MyTable",
+  name: 'MyTable',
   props: {
     arr: Array,
   },
 };
 </script>
+<style scoped lang="less">
+.my-goods-list {
+  .badge {
+    margin-right: 5px;
+  }
+}
+</style>
--- src/views/MyGoodsList.vue
+++ src/views/MyGoodsList.vue
@@ -1,9 +1,60 @@
 <template>
-  <div>MyGoodsList.vue</div>
+  <div>
+    <MyTable :arr="list">
+      <template #header>
+        <th>#</th>
+        <th>商品名称</th>
+        <th>价格</th>
+        <th>标签</th>
+        <th>操作</th>
+      </template>
+      <template #table="scope">
+        <td>{{ scope.row.id }}</td>
+        <td>{{ scope.row.goods_name }}</td>
+        <td>{{ scope.row.goods_price }}</td>
+        <td>
+          <span
+            style="margin-right: 10px"
+            class="badge badge-info"
+            v-for="(item, index) in scope.row.tags"
+            :key="index"
+            >{{ item }}</span
+          >
+        </td>
+        <td>
+          <button class="btn btn-danger btn-sm">删除</button>
+        </td>
+      </template>
+    </MyTable>
+  </div>
 </template>
 
 <script>
-export default {};
+import MyTable from '../components/MyTable.vue';
+export default {
+  data() {
+    return {
+      list: [],
+    };
+  },
+  components: {
+    MyTable,
+  },
+  created() {
+    this.$axios({
+      url: '/api/goods',
+    }).then((res) => {
+      // console.log(res.data.data);
+      this.list = res.data.data;
+    });
+  },
+};
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.my-goods-list {
+  .badge {
+    margin-right: 5px;
+  }
+}
+</style>
