f851687bf4f8ff202d6bb2554de316d59b94bd66
kelly227800 <2529303733@qq.com>
2022-07-08 14:57:23 +0800

    练习了自定义案例，新建了TabBar.vue保存TabBar案例App.vue代码

---
+++ src/TabBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <MyHeader title="tabbar"></MyHeader>
+    <component :is="comName" style="margin-top:45px"></component>
+    <MyTabBar :list="tabList" @changeTab="Fn"></MyTabBar>
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue'
+import MyTabBar from './components/MyTabBar.vue'
+import MyGoodsList from './views/MyGoodsList.vue'
+import MyGoodsSearch from './views/MyGoodsSearch.vue'
+import MyUserInfo from './views/MyUserInfo.vue'
+export default {
+  name: 'App',
+  components: {
+    MyHeader,
+    MyTabBar,
+    MyGoodsList,
+    MyGoodsSearch,
+    MyUserInfo,
+  },
+  data() {
+    return {
+      tabList: [
+        {
+          iconText: 'icon-shangpinliebiao',
+          text: '商品列表',
+          componentName: 'MyGoodsList',
+        },
+        {
+          iconText: 'icon-sousuo',
+          text: '商品搜索',
+          componentName: 'MyGoodsSearch',
+        },
+        {
+          iconText: 'icon-user',
+          text: '我的信息',
+          componentName: 'MyUserInfo',
+        },
+      ],
+      comName: 'MyGoodsList'
+    }
+  },
+  methods: {
+    Fn(val) {
+      this.comName = val
+    }
+  }
+}
+</script>
+
+<style></style>
