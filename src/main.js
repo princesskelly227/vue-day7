189c0026abb29b2a4b343dc98b26b2171df10438
kelly227800 <2529303733@qq.com>
2022-07-08 14:43:09 +0800

    练习了tabbar案例，拓展了功能

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.css"
+import "./assets/fonts/fonts/iconfont.css"
+import axios from "axios";
+axios.defaults.baseURL = "https://www.escook.cn";
+Vue.prototype.$axios = axios
+Vue.directive("focus", {
+  inserted(el){
+    el.focus()
+  }
+})
 
 Vue.config.productionTip = false
 
