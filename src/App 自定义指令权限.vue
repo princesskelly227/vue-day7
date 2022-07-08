1b14853946c9fd8f944c1a92a7a5f0ef7f0ef554
lxh15 <1392649188@qq.com>
2022-07-08 10:43:54 +0800

    自定义指令，按钮权限优化完成

--- src/App 自定义指令权限.vue
+++ src/App 自定义指令权限.vue
@@ -9,14 +9,42 @@
 <script>
 // 在 localStorage 本地缓存中 创建key为 perm  值为['add','view','update']
 export default {
+  data() {
+    return {
+      arr: ['add', 'view', 'update'],
+    };
+  },
   directives: {
-    hasProm(el, binding) {
-      //拿到当前用户的所有的权限  假设权限列表['add','view','update']
-      const perm = localStorage.getItem('perm');
-      //当前页面需要的权限
-      const needPerm = binding.value;
-      //判读是否有权限
-      const hasPermission = perm.includes(needPerm);
+    // 复杂写法
+    // hasProm(el, binding, vnode) {
+    //   // console.log(binding, vnode);
+    //   // //拿到当前用户的所有的权限  假设权限列表['add','view','update']
+    //   // // const perm = localStorage.getItem('perm');
+    //   // const perm = vnode.context.arr;
+
+    //   // //当前页面需要的权限
+    //   // const needPerm = binding.value;
+
+    //   // //判读是否有权限
+    //   // const hasPermission = perm.includes(needPerm);
+
+    //   //如果没有权限 就将该按钮隐藏 这里需要做一个延时处理，因为我们需要等待dom加载结束再进行处理Node节点
+    //   if (!hasPermission) {
+    //     setTimeout(() => {
+    //       // console.log(el.parentNode.removeChild(el));
+    //       el.style.display = 'none';
+    //       console.log(el);
+    //     }, 0);
+    //   }
+    // },
+
+    // 简化写法
+    hasProm(el, binding, vnode) {
+      // console.log(binding, vnode);
+      // //拿到当前用户的所有的权限  假设权限列表['add','view','update']
+      // //当前页面需要的权限  判读按钮是否有权限
+
+      const hasPermission = vnode.context.arr.includes(binding.value);
       //如果没有权限 就将该按钮隐藏 这里需要做一个延时处理，因为我们需要等待dom加载结束再进行处理Node节点
       if (!hasPermission) {
         setTimeout(() => {
